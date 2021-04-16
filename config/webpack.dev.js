bae4a97b048db82508e2572dc1aeb1b0c820921c
Pedro Sanchez <pedrozo_96@hotmail.com>
2021-04-16 20:41:07 -0300

    add webpack development server configuration

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -4,6 +4,11 @@ const common = require("./webpack.common");
 /**@type {import('webpack').Configuration} */
 const developmentConfiguration = {
   mode: "development",
+  devServer: {
+    port: 3000,
+    contentBase: "../build",
+    open: "chrome",
+  },
 };
 
 module.exports = merge(common, developmentConfiguration);
