9dbe08ea38c1313f3455f3d100f469311f1bae1a
Pedro Sanchez <pedrozo_96@hotmail.com>
2021-04-16 21:05:16 -0300

    add react-refresh-webpack-plugin plugin and configuration to keep react state data while coding a component

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,3 +1,5 @@
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
+const { HotModuleReplacementPlugin } = require("webpack");
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common");
 
@@ -8,7 +10,10 @@ const developmentConfiguration = {
     port: 3000,
     contentBase: "../build",
     open: "chrome",
+    hot: true,
   },
+  target: "web", //will ignore the default target that is the browserslist configuration, this is due to the dependency "webpack-dev-server" does not reloads the page when the code of a component changes, this will be fixed in the "webpack-dev-server" version 4
+  plugins: [new HotModuleReplacementPlugin(), new ReactRefreshWebpackPlugin()],
 };
 
 module.exports = merge(common, developmentConfiguration);
