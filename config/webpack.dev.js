b5828c8cca737d2c4bc8dcde6150ea1a19c4df8b
pr-sanchez <pedrosranchezchacon@gmail.com>
2026-03-17 20:44:52 -0300

    Upgrade deps for Node 20
    - Upgrade webpack, loaders, and dev-server for Node 20 compatibility
    - Update webpack-dev-server config for v4 (contentBase → static)
    - Add npm run dev script

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,5 +1,4 @@
 const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
-const { HotModuleReplacementPlugin } = require("webpack");
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common");
 
@@ -8,13 +7,12 @@ const developmentConfiguration = {
   mode: "development",
   devServer: {
     port: 3000,
-    contentBase: "../build",
-    open: "chrome",
+    static: "../build",
+    open: true,
     hot: true,
     historyApiFallback: true,
   },
-  target: "web", // will ignore the default target that is the browserslist configuration, this is due to the dependency "webpack-dev-server" does not reloads the page when the code of a component changes, this will be fixed in the "webpack-dev-server" version 4
-  plugins: [new HotModuleReplacementPlugin(), new ReactRefreshWebpackPlugin()],
+  plugins: [new ReactRefreshWebpackPlugin()],
   devtool: "eval-source-map",
   module: {
     rules: [
